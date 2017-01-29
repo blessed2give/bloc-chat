69a6f0c5e6423acd3b4f900611d36d4cc1e710c2
Paul Cho <pcho4@uic.edu>
2017-01-29 13:13:20 -0600

    bloc chat user story 6 draft send msgs

--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-    function RoomCtrl(Room, Message, $stateParams) {
+    function RoomCtrl(Room, Message, $stateParams, $cookies) {
         this.messagesByRoom = Message.getByRoomId($stateParams.id);
         var that = this;
         Room.getRoomName($stateParams.id)
@@ -7,10 +7,17 @@
             .then(function(data) {
                 that.currentRoom = data[0];
             });
+        this.messageObj = {
+            roomId: $stateParams.id,
+            content: this.msgContent,
+            sendAt: '5:50 pm',
+            username: $cookies.get('username')
+        };
         
+        this.send = Message.send(this.messageObj);
     }
     
     angular
         .module('blocChat')
-        .controller('RoomCtrl', ['Room', 'Message', '$stateParams', RoomCtrl]);
+        .controller('RoomCtrl', ['Room', 'Message', '$stateParams', '$cookies', RoomCtrl]);
 })();
--- dist/scripts/services/Message.js
+++ dist/scripts/services/Message.js
@@ -8,7 +8,15 @@
         // Filter the messages by their room ID.
         var filteredMsgsRef = ref.orderByChild('roomId').equalTo(roomId);
         return $firebaseArray(filteredMsgsRef);
-      }
+      },
+        send: function (newMessage) {
+            messages.$add({
+                content: newMessage.content,
+                roomId: newMessage.roomId,
+                sendAt: newMessage.sendAt,
+                username: newMessage.username
+            });
+        }
     };
   }
 
